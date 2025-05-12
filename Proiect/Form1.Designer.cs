2f38547397c617b653e2dd8c75d26e5f70dabf89
StoianMarioDaniel <mario-daniel.stoian@student.tuiasi.ro>
2025-05-13 00:51:56 +0300

    Initial Project

---
+++ Proiect/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Proiect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.gmapControl = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Locație Plecare:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(425, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Locație Sosire:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(558, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(166, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(191, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(820, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Viteză (km/h)";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(820, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Timp estimat:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(820, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Consum estimat (L)";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(946, 22);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(946, 67);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(946, 119);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(313, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Calculează Ruta";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // gmapControl
+            // 
+            this.gmapControl.Bearing = 0F;
+            this.gmapControl.CanDragMap = true;
+            this.gmapControl.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gmapControl.GrayScaleMode = false;
+            this.gmapControl.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+      //      this.gmapControl.LevelsKeepInMemmory = 5;
+            this.gmapControl.Location = new System.Drawing.Point(59, 195);
+            this.gmapControl.MarkersEnabled = true;
+            this.gmapControl.MaxZoom = 2;
+            this.gmapControl.MinZoom = 2;
+            this.gmapControl.MouseWheelZoomEnabled = true;
+            this.gmapControl.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gmapControl.Name = "gmapControl";
+            this.gmapControl.NegativeMode = false;
+            this.gmapControl.PolygonsEnabled = true;
+            this.gmapControl.RetryLoadTile = 0;
+            this.gmapControl.RoutesEnabled = true;
+            this.gmapControl.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gmapControl.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gmapControl.ShowTileGridLines = false;
+            this.gmapControl.Size = new System.Drawing.Size(987, 357);
+            this.gmapControl.TabIndex = 11;
+            this.gmapControl.Zoom = 0D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1170, 601);
+            this.Controls.Add(this.gmapControl);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private GMap.NET.WindowsForms.GMapControl gmapControl;
+    }
+}
+
---
+++ Proiect/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Forms; 
+using GMap.NET;           
+using GMap.NET.MapProviders; 
+using GMap.NET.WindowsForms; 
+
+
+namespace Proiect
+{
+    public partial class Form1 : Form
+    {
+        private GMapOverlay markersOverlay;
+        private GMapOverlay routesOverlay;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                gmapControl.MapProvider = GMapProviders.OpenStreetMap;
+
+                gmapControl.MinZoom = 1;  
+                gmapControl.MaxZoom = 18; 
+
+                gmapControl.Zoom = 6;
+
+
+                gmapControl.DragButton = MouseButtons.Left;
+
+
+                gmapControl.Position = new PointLatLng(45.9432, 24.9668); 
+
+
+                gmapControl.ShowCenter = false;
+
+                if (gmapControl.Overlays.Count == 0)
+                {
+                    markersOverlay = new GMapOverlay("markers"); 
+                    gmapControl.Overlays.Add(markersOverlay);
+
+                    routesOverlay = new GMapOverlay("routes");  
+                    gmapControl.Overlays.Add(routesOverlay);
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show($"A apărut o eroare la inițializarea hărții: {ex.Message}",
+                                "Eroare GMap.NET",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+
+        }
+
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+
+    }
+}

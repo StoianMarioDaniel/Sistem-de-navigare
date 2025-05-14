54bd5939c5c7eb71c85cbd1480c2d082c55ef27b
StoianMarioDaniel <mario-daniel.stoian@student.tuiasi.ro>
2025-05-14 21:18:19 +0300

    Implementare Task 5 - Calcul Timp și Consum

--- Proiect/Form1.Designer.cs
+++ Proiect/Form1.Designer.cs
@@ -45,102 +45,98 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(42, 39);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(56, 48);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.Size = new System.Drawing.Size(104, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Locație Plecare:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(319, 39);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(425, 48);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.Size = new System.Drawing.Size(96, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Locație Sosire:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(418, 39);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Location = new System.Drawing.Point(557, 48);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(124, 20);
+            this.textBox1.Size = new System.Drawing.Size(164, 22);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(124, 39);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Location = new System.Drawing.Point(165, 48);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(144, 20);
+            this.textBox2.Size = new System.Drawing.Size(191, 22);
             this.textBox2.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(615, 18);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(820, 22);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.Size = new System.Drawing.Size(84, 16);
             this.label3.TabIndex = 4;
             this.label3.Text = "Viteză (km/h)";
-           // 
+            // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(615, 54);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(820, 66);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.Size = new System.Drawing.Size(87, 16);
             this.label4.TabIndex = 5;
             this.label4.Text = "Timp estimat:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(615, 97);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(820, 119);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(96, 13);
+            this.label5.Size = new System.Drawing.Size(120, 16);
             this.label5.TabIndex = 6;
             this.label5.Text = "Consum estimat (L)";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(710, 18);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Location = new System.Drawing.Point(947, 22);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(76, 20);
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
             this.textBox3.TabIndex = 7;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(710, 54);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox4.Location = new System.Drawing.Point(947, 66);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(76, 20);
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
             this.textBox4.TabIndex = 8;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(710, 97);
-            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox5.Location = new System.Drawing.Point(947, 119);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(76, 20);
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
             this.textBox5.TabIndex = 9;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(235, 91);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(313, 112);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 19);
+            this.button1.Size = new System.Drawing.Size(147, 23);
             this.button1.TabIndex = 10;
             this.button1.Text = "Calculează Ruta";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // gmapControl
             // 
@@ -150,8 +146,8 @@
             this.gmapControl.GrayScaleMode = false;
             this.gmapControl.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.gmapControl.LevelsKeepInMemory = 5;
-            this.gmapControl.Location = new System.Drawing.Point(44, 158);
-            this.gmapControl.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gmapControl.Location = new System.Drawing.Point(59, 194);
+            this.gmapControl.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gmapControl.MarkersEnabled = true;
             this.gmapControl.MaxZoom = 2;
             this.gmapControl.MinZoom = 2;
@@ -165,15 +161,15 @@
             this.gmapControl.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.gmapControl.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.gmapControl.ShowTileGridLines = false;
-            this.gmapControl.Size = new System.Drawing.Size(740, 290);
+            this.gmapControl.Size = new System.Drawing.Size(987, 357);
             this.gmapControl.TabIndex = 11;
             this.gmapControl.Zoom = 0D;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(878, 488);
+            this.ClientSize = new System.Drawing.Size(1171, 601);
             this.Controls.Add(this.gmapControl);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox5);
@@ -186,7 +182,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
--- Proiect/Form1.cs
+++ Proiect/Form1.cs
@@ -1,5 +1,5 @@
-﻿using GMap.NET;           
-using GMap.NET.MapProviders; 
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
 using GMap.NET.WindowsForms;
 using GMap.NET.WindowsForms.Markers;
 using Newtonsoft.Json.Linq;
@@ -7,8 +7,18 @@ using System;
 using System.Drawing;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Windows.Forms; 
+using System.Windows.Forms;
 
+
+
+
+/*********************************************************************************************
+ *                                                                                           * 
+ *  File:        RouteCalculator.cs                                                          *
+ *  Copyright:   (c) 2025 Stoian Mario-Daniel, Chiriac Raluca                                *
+ *  E-mail:      mario-daniel.stoian@student.tuiasi.ro, raluca.chiriac@student.tuiasi.ro     *
+ *                                                                                           *
+ *********************************************************************************************/
 
 
 namespace Proiect
@@ -21,29 +31,18 @@ namespace Proiect
         public Form1()
         {
             InitializeComponent();
-
         }
-
+        //---------SMD-----------
         private void Form1_Load(object sender, EventArgs e)
         {
-
             try
             {
-
                 gmapControl.MapProvider = GMapProviders.OpenStreetMap;
-
                 gmapControl.MinZoom = 1;
                 gmapControl.MaxZoom = 18;
-
                 gmapControl.Zoom = 6;
-
-
                 gmapControl.DragButton = MouseButtons.Left;
-
-
                 gmapControl.Position = new PointLatLng(45.9432, 24.9668);
-
-
                 gmapControl.ShowCenter = false;
 
                 if (gmapControl.Overlays.Count == 0)
@@ -54,22 +53,18 @@ namespace Proiect
                     routesOverlay = new GMapOverlay("routes");
                     gmapControl.Overlays.Add(routesOverlay);
                 }
-
-
-
             }
             catch (Exception ex)
             {
-
                 MessageBox.Show($"A apărut o eroare la inițializarea hărții: {ex.Message}",
                                 "Eroare GMap.NET",
                                 MessageBoxButtons.OK,
                                 MessageBoxIcon.Error);
             }
-
         }
+        //---------SMD-----------/
 
-        //functia care preia coordonatele de la adresa 
+
         private async Task<PointLatLng?> GetCoordinatesAsync(string address)
         {
             if (string.IsNullOrWhiteSpace(address))
@@ -94,32 +89,51 @@ namespace Proiect
                     if (results.Count == 0)
                         return null;
 
-                    double lat = double.Parse((string)results[0]["lat"]);
-                    double lon = double.Parse((string)results[0]["lon"]);
+                    if (results[0]["lat"] == null || results[0]["lon"] == null)
+                        return null;
+
+                    if (!double.TryParse((string)results[0]["lat"], System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out double lat) ||
+                        !double.TryParse((string)results[0]["lon"], System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out double lon))
+                    {
+                        Console.WriteLine($"Eroare la parsarea coordonatelor pentru adresa: {address}. Lat: {results[0]["lat"]}, Lon: {results[0]["lon"]}");
+                        return null;
+                    }
 
                     return new PointLatLng(lat, lon);
                 }
-                catch
+                catch (Exception ex)
                 {
+                    Console.WriteLine($"Excepție în GetCoordinatesAsync pentru '{address}': {ex.Message}");
                     return null;
                 }
             }
         }
 
-
         private async void button1_Click(object sender, EventArgs e)
         {
-            var coordPlecare = await GetCoordinatesAsync(textBox1.Text);
-            var coordSosire = await GetCoordinatesAsync(textBox2.Text);
+            var coordPlecare = await GetCoordinatesAsync(textBox2.Text);
+            var coordSosire = await GetCoordinatesAsync(textBox1.Text);
 
-            if (coordPlecare == null || coordSosire == null)
+            if (coordPlecare == null)
             {
-                MessageBox.Show("Una dintre locații nu a putut fi găsită.", "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Locația de plecare nu a putut fi găsită sau nu a fost introdusă.", "Eroare Locație Plecare", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
+            //---------SMD-----------
+            if (coordSosire == null)
+            {
+                MessageBox.Show("Locația de sosire nu a putut fi găsită sau nu a fost introdusă.", "Eroare Locație Sosire", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //---------SMD-----------/
 
             markersOverlay.Markers.Clear();
             routesOverlay.Routes.Clear();
+
+            //---------SMD-----------
+            textBox4.Text = "";
+            textBox5.Text = "";
+            //---------SMD-----------/
 
             markersOverlay.Markers.Add(new GMarkerGoogle(coordPlecare.Value, GMarkerGoogleType.green));
             markersOverlay.Markers.Add(new GMarkerGoogle(coordSosire.Value, GMarkerGoogleType.red));
@@ -127,24 +141,95 @@ namespace Proiect
             var routingService = new RoutingService();
             var routes = await routingService.GetRoutesAsync(coordPlecare.Value, coordSosire.Value);
 
-            if (routes.Count == 0)
+            if (routes == null || routes.Count == 0)
             {
-                MessageBox.Show("Nu a putut fi găsită nicio rută!", "Atenție!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show("Nu a putut fi găsită nicio rută între locațiile specificate!", "Atenție!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
 
-            int colorStep = 255 / routes.Count;
+            //---------SMD-----------
+            double averageSpeedKmH = 0;
+            bool canCalculateMetrics = false;
+            string originalSpeedInput = textBox3.Text;
+
+            if (string.IsNullOrWhiteSpace(originalSpeedInput))
+            {
+                averageSpeedKmH = 60.0; 
+                canCalculateMetrics = true;
+            }
+            else if (double.TryParse(originalSpeedInput, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out double parsedSpeed) && parsedSpeed > 0)
+            {
+                averageSpeedKmH = parsedSpeed; 
+                canCalculateMetrics = true;
+            }
+
+            if (canCalculateMetrics) 
+            {
+
+                textBox3.Text = averageSpeedKmH.ToString("G", System.Globalization.CultureInfo.InvariantCulture);
+
+                if (routes.Count > 0)
+                {
+                    const double fuelConsumptionRateL100Km = 7.5;
+                    var firstRouteInfo = routes[0];
+
+                    try
+                    {
+                        var metrics = RouteCalculator.CalculateRouteMetrics(firstRouteInfo, averageSpeedKmH, fuelConsumptionRateL100Km);
+
+                        TimeSpan calculatedTime = metrics.CalculatedEstimatedTime;
+                        string formattedCalculatedTime = $"{(int)calculatedTime.TotalHours}h {calculatedTime.Minutes:D2}m";
+                        textBox4.Text = formattedCalculatedTime;
+
+                        textBox5.Text = metrics.EstimatedConsumptionLiters.ToString("F2", System.Globalization.CultureInfo.InvariantCulture) + " L";
+
+                        TimeSpan apiTime = metrics.ApiProvidedDuration;
+                        string formattedApiTime = $"{(int)apiTime.TotalHours}h {apiTime.Minutes:D2}m {apiTime.Seconds:D2}s";
+                        Console.WriteLine($"--- Detalii Rută (Prima) ---");
+                        Console.WriteLine($"Distanță: {metrics.DistanceKm.ToString("F2", System.Globalization.CultureInfo.InvariantCulture)} km");
+                        Console.WriteLine($"Timp estimat (calculat cu {averageSpeedKmH.ToString("G", System.Globalization.CultureInfo.InvariantCulture)} km/h): {formattedCalculatedTime}");
+                        Console.WriteLine($"Durată (din API OSRM): {formattedApiTime}");
+                        Console.WriteLine($"Consum estimat ({fuelConsumptionRateL100Km} L/100km): {metrics.EstimatedConsumptionLiters.ToString("F2", System.Globalization.CultureInfo.InvariantCulture)} L");
+                        Console.WriteLine($"-----------------------------");
+                    }
+                    catch (ArgumentOutOfRangeException exMetrics)
+                    {
+                        MessageBox.Show($"Eroare la calcularea metricilor: {exMetrics.Message}", "Eroare Calcul", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        textBox4.Text = "";
+                        textBox5.Text = "";
+                    }
+                    catch (Exception exGen)
+                    {
+                        MessageBox.Show($"O eroare neașteptată la calcularea metricilor: {exGen.Message}", "Eroare Calcul General", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        textBox4.Text = "";
+                        textBox5.Text = "";
+                    }
+                }
+            }
+            //---------SMD-----------/
+
+
+            int colorStep = routes.Count > 1 ? 255 / (routes.Count - 1) : 255;
             for (int i = 0; i < routes.Count; i++)
             {
                 var route = routes[i];
-                var routeLine = new GMapRoute(route.Geometry, $"Ruta {i + 1}")
+                var routeLine = new GMapRoute(route.Geometry, $"Ruta {i + 1} ({(route.Distance / 1000.0).ToString("F1")} km)")
                 {
-                    Stroke = new Pen(Color.FromArgb(255, i * colorStep, 0, 255 - i * colorStep), 3)
+                    Stroke = routes.Count == 1 ? new Pen(Color.Blue, 3) :
+                             new Pen(Color.FromArgb(255, Math.Min(255, i * colorStep), Math.Max(0, 100 - i * (colorStep / 2)), Math.Max(0, 255 - i * colorStep)), 3)
                 };
                 routesOverlay.Routes.Add(routeLine);
             }
 
-            gmapControl.ZoomAndCenterRoutes("routes");
+            if (routes.Count > 0)
+            {
+                gmapControl.ZoomAndCenterRoutes("routes");
+            }
+            else
+            {
+                if (coordPlecare.HasValue) gmapControl.Position = coordPlecare.Value;
+                gmapControl.Zoom = 6;
+            }
         }
     }
 }
